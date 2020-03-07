fe45b9fc52bbb79d67fd2c1285107d2349f39de4
Abin Mn <abinmn619@gmail.com>
2020-03-07 20:41:27 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#Decode Base64-key to json file
+echo "$INPUT_SERVICE_KEY" | base64 --decode > "$HOME"/service_key.json
+
+gcloud auth activate-service-account --key-file="$HOME"/gcloud.json --project "$INPUT_PROJECT"
+
+gsutil rsync -R "$INPUT_BUILD_FOLDER" gs://"$INPUT_BUCKET_NAME"
+gsutil web set -m "$INPUT_HOME_PAGE_PATH" -e "$INPUT_ERROR_PAGE_PATH" gs://www.excelmec.org
